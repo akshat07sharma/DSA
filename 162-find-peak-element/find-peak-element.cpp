1306f9375b9d7a876490fbcccaa1c84e5d215d3b
akshat07sharma <akshatsharma56923@gmail.com>
2026-06-02 19:06:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (5.52%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& arr) {
-        int start = 1;
-        int end = arr.size()-2;
-        int mid = start+(end-start)/2;
-        int n = arr.size();
+      int n = arr.size();
+      int start = 1;
+      int end = n-2;
 
-        if (n == 1) return 0;
-        if(arr[0] > arr[1]) return 0;
+      if(n==1) return 0;
 
-        if (arr[n - 1] > arr[n - 2]) return n - 1;
+      if(arr[n-1]>arr[n-2]) return n-1;
 
-        while(start<=end){
-            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
-                return mid;
-            }
-            if(arr[mid]<arr[mid+1]){
-                start = mid+1;
-            }
-            else{
-                end = mid-1;
-            }
-            mid = start+(end-start)/2;
-        }
+      if(arr[0]>arr[1]) return 0;
 
-        return -1;
+      if(n==2 && arr[0]<arr[1]) return 1;
+      
+      while(start<=end){
+        int mid = start + (end-start)/2;
+
+        if(arr[mid]>arr[mid-1]&& arr[mid]>arr[mid+1]) return mid;
+
+        else if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1]) start = mid+1;
+
+        else end = mid-1;
+      }
+     return -1;
+
     }
 };

8dbe641723c4053614267ee17c3c46e62de056a4
akshat07sharma <akshatsharma56923@gmail.com>
2026-06-03 10:24:17 +0530

    Time: 31 ms (23.66%) | Memory: 23.8 MB (83.01%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+     int n = s.length();
+     int len = words[0].size();
+     vector<int>ans;
+
+     unordered_map<string,int>map;
+     for(string str:words){
+        map[str]++;
+     }
+     for(int i = 0;i<len;i++){
+        int left = i;
+        int right = i;
+        unordered_map<string,int>winMap;
+        int count = 0;
+        while(right<=n-len){
+            string temp = s.substr(right,len);
+            if(map.find(temp)!= map.end()){
+                winMap[temp]++;
+                count++;
+                while(winMap[temp]>map[temp]){
+                    string leftstr = s.substr(left,len);
+                    winMap[leftstr]--;
+                    left+= len;
+                    count--;
+                }
+                if(count == words.size()) ans.push_back(left);
+
+            }
+            else{
+                left = right+len;
+                count = 0;
+                winMap.clear();
+            }
+            right += len;
+        }
+     }
+     return ans;
+    }
+};

706e20c94c127afcfdca0cc6a0d084eeea3e4b58
akshat07sharma <akshatsharma56923@gmail.com>
2026-06-15 11:02:33 +0530

    Time: 433 ms (30.78%) | Memory: 224 MB (20.98%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+
+    using vi = vector<int>;
+    using vvi = vector<vector<int>>;
+    using vvvi = vector<vector<vector<int>>>;
+
+    int helper(vector<int>& prices, int day, int state,
+               int k, vvvi& memo) {
+
+        if(day >= prices.size() || k == 0)
+            return 0;
+
+        if(memo[day][state][k] != -1)
+            return memo[day][state][k];
+
+        int profit = 0;
+
+        if(state == 0) {
+            int buy = -prices[day] +
+                      helper(prices, day + 1, 1, k, memo);
+
+            int dontbuy =
+                      helper(prices, day + 1, 0, k, memo);
+
+            profit = max(buy, dontbuy);
+        }
+        else {
+            int hold =
+                      helper(prices, day + 1, 1, k, memo);
+
+            int sell = prices[day] +
+                       helper(prices, day + 1, 0, k - 1, memo);
+
+            profit = max(hold, sell);
+        }
+
+        return memo[day][state][k] = profit;
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int k = 2;
+
+        int n = prices.size();
+
+        vvvi memo(n, vvi(2, vi(k + 1, -1)));
+
+        return helper(prices, 0, 0, k, memo);
+    }
+};
